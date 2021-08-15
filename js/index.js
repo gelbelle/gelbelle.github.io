afcc65c29db5bbe0527a40055b02595fa9e66d90
Angeleah <a.hoeppner@yahoo.ca>
2021-08-15 12:20:45 -0700

    Delete js directory

--- js/index.js
+++
@@ -1,9 +0,0 @@
-$('.navbar-nav a').on('click', function () {
-        if (window.innerWidth <= 768) {
-            $(".navbar-toggle").click();
-        }
-    });
-
-$(".modal").on('shown', function() {
-    $(this).find("[autofocus]:first").focus();
-});
